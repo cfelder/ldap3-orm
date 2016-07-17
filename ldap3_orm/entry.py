14e172e072c4d2f545d2bdec33b0113f2d7d01e8
Christian Felder <c.felder@fz-juelich.de>
2016-07-17 23:38:44 +0200

    Setup initial project structure.
    Change-Id: I277b0786d389eac7e0e7a5af3c96e4cf7bf59cbb

---
+++ ldap3_orm/entry.py
@@ -0,0 +1,85 @@
+# coding: utf-8
+
+from ldap3 import Attribute, AttrDef
+from ldap3 import SEQUENCE_TYPES
+from ldap3 import Entry as _Entry
+from six import add_metaclass
+# pylint: disable=unused-import
+from ldap3_orm._version import __version__, __revision__
+
+
+__author__ = "Christian Felder <webmaster@bsm-felder.de>"
+__copyright__ = """Copyright 2016, Christian Felder
+
+This file is part of ldap3-orm, object-relational mapping for ldap3.
+
+ldap3-orm is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ldap3-orm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with ldap3-orm. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+class EntryMeta(type):
+
+    def __init__(cls, name, bases, attrs):
+        type.__init__(cls, name, bases, attrs)
+        if not hasattr(cls, "_attrdefs"):
+            cls._attrdefs = {}
+        # for class inheritance first merge and/or update
+        # all _attrdefs in correct order
+        newattrdefs = {}
+        for base in reversed(bases):
+           if hasattr(base, "_attrdefs"):
+               newattrdefs.update(base._attrdefs)
+        # merge and/or update _attrdefs for current class
+        for k, attr in attrs.iteritems():
+            if isinstance(attr, AttrDef):
+                newattrdefs[k] = attr
+                delattr(cls, k)
+        cls._attrdefs = newattrdefs
+
+
+@add_metaclass(EntryMeta)
+class Entry(_Entry):
+
+    def __init__(self, **kwargs):
+        _Entry.__init__(self, "cn=" + kwargs.get("username") + ','
+                        + self.base_dn, None)
+        # initialize attributes from kwargs
+        attrdefs = dict(self._attrdefs)
+        for k, v in kwargs.iteritems():
+            if k in self._attrdefs:
+                attrdef = attrdefs.pop(k)
+                self._create_attribute(attrdef, v)
+            else:
+                raise TypeError("__init__() got an unexpected keyword argument"
+                                " '%s'" % k)
+        # check remaining attributes
+        for key in attrdefs.keys():
+            if attrdefs[key].default != NotImplemented:
+                attrdef = attrdefs.pop(key)
+                self._create_attribute(attrdef, attrdef.default)
+        # all remaining attributes have no reasonable default value
+        # (NotImplemented) and should have been set earlier
+        if attrdefs:
+             s = " '" if len(attrdefs) == 1 else "s '"
+             raise TypeError("__init__() missing the following keyword "
+                             "argument" + s + ", ".join(attrdefs.keys()) + "'")
+
+    def _create_attribute(self, attrdef, value):
+        tolist = lambda itm: itm if isinstance(itm, SEQUENCE_TYPES) \
+                                 else [itm]
+        attribute = Attribute(attrdef, self)
+        # TODO: check for validator and validate
+        attribute.__dict__["values"] = tolist(value)
+        self.__dict__["_attributes"][attribute.key] = attribute
---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import os
+import os.path
+from distutils.core import Command
+from setuptools import find_packages, setup
+import vcversioner
+
+
+__author__ = "Christian Felder <webmaster@bsm-felder.de>"
+__version__ = vcversioner.find_version(
+                   version_module_paths=[os.path.join("ldap3_orm",
+                                                      "_version.py")]).version
+__copyright__ = """Copyright 2016, Christian Felder
+
+This file is part of ldap3-orm, object-relational mapping for ldap3.
+
+ldap3-orm is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ldap3-orm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with ldap3-orm. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+class test(Command):
+
+    description = "run nosetest test suite"
+
+    # List of option tuples: long name, short name (None if no short
+    # name), and help string.
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import nose
+        import ldap3_orm
+        print("ldap3-orm version:", ldap3_orm.__version__)
+        print()
+        nose.run(argv=[sys.argv[0], "test.ldap3_orm", "-v"])
+
+
+setup(cmdclass={"test": test},
+      name="ldap3-orm",
+      version=__version__,
+      description="ldap3-orm, object-relational mapping for ldap3",
+      author="Christian Felder",
+      author_email="webmaster@bsm-felder.de.de",
+      license="LGPL-3.0+",
+      packages=find_packages(exclude=["test"]),
+      include_package_data=True,
+      requires=["ldap3"],
+     )
